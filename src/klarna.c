cb040e90d4c0d6c54d81b508b5f5c3c2bb232f5c
Uwe Dauernheim <uwe@klarna.com>
2011-08-01 09:40:28 +0200

    Whitespace and line-length cleanup

--- src/klarna.c
+++ src/klarna.c
@@ -1,8 +1,3 @@
-/*
-  Design decisions:
-   * No float: Extra initialization space
-   * No math: Extra include header file space
- */
 #include <stdio.h>
 #include <math.h>
 #include <sysexits.h>
@@ -16,9 +11,9 @@ static const int B   =  1;   // Circle border size
 static const int X   = 23;   // X-Translation
 static const int Y   =  7;   // Y-Translation
 static const float S =  0.5; // Scale
-static const int K[] = {' ', '.', '.', ')', '.', '\\', '/', '@'}; // Pixel "brightness"
-static const int C[] = {      // Circles' data, leaves in background to foreground order
-// x, y, r
+static const int K[] = {' ', '.', '.', ')', '.', '\\', '/', '@'}; // Brightness
+static const int C[] = {     // Circles' data, leaf order: back- to foreground
+// x,  y,  r
    0, 21, 48, // < yellow
   74, 11, 41, // > yellow
    0, 10, 51, // < green
@@ -34,7 +29,8 @@ static const int C[] = {      // Circles' data, leaves in background to foregrou
   43,  6, 24, // < black
   78, 37, 32  // > black
 };
-static const int colors[] = {3, 2, 4, 6, 1, 5, 7}; // yellow, green, blue, cyan, red, purple, black
+                         //  y, g, b, c, r, p, b
+static const int colors[] = {3, 2, 4, 6, 1, 5, 7};
 
 int main(int argc, char *argv[])
 {
@@ -58,34 +54,40 @@ int main(int argc, char *argv[])
     {
       for (x = 0; x < W*3; x+=3) // X-Axis loop
       {
-	j = 0; // Initial brightness: dark
-	color = 0; // Default colour is black
-	for (l = 0; l < A; l++) // Sampling loop
-	{
-	  k = 0;
-	  for (i = 0; i <= 7-1; i++) // Leaf loop (pick circle from back- to foreground)
-	  {
-	    for (t = 0; t <= 1; t++) // Inner/Border loop (toggle Fill & Border/Erase method)
-	    {
-	      if ( pow(Xs*(x+l) + X - C[6*6+0], 2) + pow(Ys*y + -Y - C[6*6+1], 2) > pow(C[6*6+2] - 8*(1-pow(1-(float)f/F,2)) + !t*B, 2) && // Mask to hide...
-		   pow(Xs*(x+l) + X - C[6*5+3], 2) + pow(Ys*y + -Y - C[6*5+4], 2) > pow(C[6*5+5] - 8*(1-pow(1-(float)f/F,2)) + !t*B, 2) )  // ...lower overflow
-	      {
-		continue;
-	      }
-	      if ( pow(Xs*(x+l) + X - C[6*i+0], 2) + pow(Ys*y + -Y - C[6*i+1], 2) <= pow(C[6*i+2] - 8*(1-pow(1-(float)f/F,2)) + !t*B, 2) && // Inside left...
-		   pow(Xs*(x+l) + X - C[6*i+3], 2) + pow(Ys*y + -Y - C[6*i+4], 2) <= pow(C[6*i+5] - 8*(1-pow(1-(float)f/F,2)) + !t*B, 2) )  // ...and right circle?
-	      {
-		k = t; // Update current pixel with latest value (filled or erased)
-		color = colors[i]; // Set colour of most foreground leaf
-	      }
-	    }
-	  }
-	  
-	  j ^= k << l; // Increase pixel brightness: t=1,{l=0->1, l=1->2, l=2->4}.
-	}
-	
-	//printf("\x1b[0;3%dm", color); // Colourize pixel
-	putchar(K[j]); // Print pixel with brightness
+        j = 0; // Initial brightness: dark
+        color = 0; // Default colour is black
+        for (l = 0; l < A; l++) // Sampling loop
+        {
+          k = 0;
+          for (i = 0; i <= 7-1; i++) // Leaf loop (from back- to foreground)
+          {
+            for (t = 0; t <= 1; t++) // Fill/Border loop (toggle method)
+            {
+              // Mask to hide lower overflow
+              if ( pow(Xs*(x+l) + X - C[6*6+0],2) + pow(Ys*y + -Y - C[6*6+1],2)
+                   > pow(C[6*6+2] - 8*(1-pow(1-(float)f/F,2)) + !t*B, 2) && 
+                   pow(Xs*(x+l) + X - C[6*5+3],2) + pow(Ys*y + -Y - C[6*5+4],2)
+                   > pow(C[6*5+5] - 8*(1-pow(1-(float)f/F,2)) + !t*B, 2) )
+              {
+                continue;
+              }
+              // Inside left and right circle?
+              if ( pow(Xs*(x+l) + X - C[6*i+0],2) + pow(Ys*y + -Y - C[6*i+1],2)
+                   <= pow(C[6*i+2] - 8*(1-pow(1-(float)f/F,2)) + !t*B, 2) &&
+                   pow(Xs*(x+l) + X - C[6*i+3],2) + pow(Ys*y + -Y - C[6*i+4],2)
+                   <= pow(C[6*i+5] - 8*(1-pow(1-(float)f/F,2)) + !t*B, 2) )
+              {
+                k = t; // Update pixel with latest value (filled or erased)
+                color = colors[i]; // Set colour of most foreground leaf
+              }
+            }
+          }
+          
+          j ^= k << l; // Increase pixel brightness: {l=0->1, l=1->2, l=2->4}
+        }
+        
+        //printf("\x1b[0;3%dm", color); // Colourize pixel
+        putchar(K[j]); // Print pixel with brightness
       }
       putchar('\n'); // Next line
     }
