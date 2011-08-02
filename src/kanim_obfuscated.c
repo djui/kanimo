41cea0fd2e74b5c91e8e247fefe3d9f4443fa6dd
Uwe Dauernheim <uwe@klarna.com>
2011-08-02 14:12:29 +0200

    Insert author name ;-)

--- src/kanim_obfuscated.c
+++ src/kanim_obfuscated.c
@@ -1,5 +1,5 @@
-f=50,x,y,i,j,k,l,t;C(i){return" 5Pj+I *Sm7H+FBN$5 <Pa)<4#I~?Q-PFN&8K&8nE@"[i]-32
-;}s(float x){return x*x;}c(i){return s(0.34*(x+l)+23-C(i))+s(2.0*y-7-C(i+1))<=s(
-C(i+2)-8*(1-(1-f/5e1))+t);}main(){for(;f--;f&&puts("\e[19A")&usleep(20000))for(y
-=0;y<18;y++,puts(""))for(x=0;x<138;x+=3,putchar(" ..).\\/8"[j]))for(l=j=k=0;l<3;
-j^=k<<l++)for(i=0;i<42;i+=6)for(t=2;t--;)c(33)&c(36)&c(i)&c(i+3)?k=!t:0;}
+f=50,x,y,i,j,k,l,t;C(i){return " 5Pj+I *Sm7H+FBN$5 <Pa)<4#I~?Q-PFN&8K&8nE@ Djui"
+[i]-32;}s(float x){return x*x;}c(i){return s(0.34*(x+l)+23-C(i))+s(2.0*y-7-C(i+1
+))<=s(C(i+2)-8*(1-(1-f/5e1))+t);}main(){for(;f--;f&&puts("\e[19A")&usleep(20000)
+)for(y=0;y<18;y++,puts(""))for(x=0;x<138;x+=3,putchar(" ..).\\/8"[j]))for(l=j=k=
+0;l<3;j^=k<<l++)for(i=0;i<42;i+=6)for(t=2;t--;)c(33)&c(36)&c(i)&c(i+3)?k=!t:0;}
