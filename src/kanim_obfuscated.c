eb6ec9090aef7b2cd762c17ad01b4dd3017dcb31
Uwe Dauernheim <uwe@klarna.com>
2011-08-02 02:39:01 +0200

    Started compressing the code (currently 5 lines)

--- src/kanim_obfuscated.c
+++ src/kanim_obfuscated.c
@@ -1,15 +1,25 @@
-// Good function for scale-wooble with start at scale 1, max 1.5, min ~0.75, and ease-out at ~5: y=1+sin(x*x)*exp(0.5*x)
-f=100,x,y,i,j,t,C[]=" )Qj)H  Tm3H+EAX!3 2O_+;4#I~8R+MBX%4U%4nBB";
+f=50,x,y,i,j,k,l,t;C(i){" 5Pj+I *Sm7H+FBN$5 <Pa)<4#I~?Q-PFN&8K&8nE@"[i]-32;}
+s(x){x*x;}c(i){s(0.34*(x+l)+23-C(i))+s(2.0*y-7-C(i+1))<=s(C(i+2)-8*(1-s(1-f/
+5e1))+t);}main(){for(;f--;f&&puts("\x1b[19A")&usleep(20000))for(y=0;y<18;y++
+,putchar(10))for(x=j=0;x<46*3;x+=3,putchar(" ..).\\/8"[j]))for(l=k=0;l<3;j^=
+k<<l++)for(i=0;i<42;i++)for(t=2;t--;)c(33)&c(36)&c(i)&c(i+3)?k=t:0;} // Djui
 
-s(x){return x*x;}
+/*
+f=50,x,y,i,j,k,l,t;C(i){" 5Pj+I *Sm7H+FBN$5 <Pa)<4#I~?Q-PFN&8K&8nE@"[i]-32;}
+s(x){x*x;}c(x,y,l,f,t,i){s(0.34*(x+l)+23-C(i))+s(2.0*y-7-C(++i))<s(C(++i)-8*(1-s(1-f/50.0))+t);}
+main(){for(;f--;f&&puts("\x1b[19A")&usleep(20000))
+for(y=0;y<18;y++,putchar(10))
+for(x=j=0;x<46*3;x+=3,putchar(" ..).\\/8"[j]))
+for(l=k=0;l<3;j^=k<<l++)
+for(i=0;i<42;i++)
+for(t=2;t--;)c(x,y,l,f,t,33)&&c(x,y,l,f,t,36)&&c(x,y,l,f,t,i)&&c(x,y,l,f,t,i+3)?k=t:0;}
+*/
 
-main(){
-  for (;f--,f?puts("\x1b[25A"):0)
-    for(y=0;y<25;y++,putchar(10))
-      for(x=0,j=0;x<75;x+3,putchar(" .oo@"[j]))
-        for(i=0;i<42;i+6)
-	  for(t=2;t--;)
-            if(s(x-C[i  ]-32)+s(y-C[i+1]-32) <= s(t*50+C[i+2]-32)&&
-	       s(x-C[i+3]-32)+s(y-C[i+4]-32) <= s(t*50+C[i+5]-32))
-              j = t?j>>1:j<<1;
-}
+/*
+c,p,i,j,n,F=40,k,m;float a,x,y,S=0,V=0;main(){for(;F--;usleep(50000),F?puts(
+"\x1b[25A"):0)for(S+=V+=(1-S)/10-V/4,j=0;j<72;j+=3,putchar(10))for(i=0;x=S*(
+i-27),i++<73;putchar(c[" ''\".$u$"]))for(c=0,n=3;n--;)for(y=S*(j+n-36),k=0,c
+^=(136*x*x+84*y*y<92033)<<n,p=6,m=0;m<8;k++["<[\\]O=IKNAL;KNRbF8EbGEROQ@BSX"
+"XtG!#t3!^"]/1.16-68>x*cos(a)+y*sin(a)?k=p,p="<AFJPTX"[m++]-50:k==p?c^=1<<n,
+m=8:0)a=(k["O:85!fI,wfO8!yZfO8!f*hXK3&fO;:O;#hP;\"i[by asloane"]-79)/14.64;}
+*/
