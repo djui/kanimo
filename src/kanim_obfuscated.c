cccc2f56bbf30e28f643a50065f6aaa8880fae90
Uwe Dauernheim <uwe@klarna.com>
2011-08-02 14:14:11 +0200

    Change author name

--- src/kanim_obfuscated.c
+++ src/kanim_obfuscated.c
@@ -1,4 +1,4 @@
-f=50,x,y,i,j,k,l,t;C(i){return " 5Pj+I *Sm7H+FBN$5 <Pa)<4#I~?Q-PFN&8K&8nE@ Djui"
+f=50,x,y,i,j,k,l,t;C(i){return" 5Pj+I *Sm7H+FBN$5 <Pa)<4#I~?Q-PFN&8K&8nE@by Uwe"
 [i]-32;}s(float x){return x*x;}c(i){return s(0.34*(x+l)+23-C(i))+s(2.0*y-7-C(i+1
 ))<=s(C(i+2)-8*(1-(1-f/5e1))+t);}main(){for(;f--;f&&puts("\e[19A")&usleep(20000)
 )for(y=0;y<18;y++,puts(""))for(x=0;x<138;x+=3,putchar(" ..).\\/8"[j]))for(l=j=k=
