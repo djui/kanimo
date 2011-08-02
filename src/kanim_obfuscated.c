55fcf751f1137a073b621417031b4e37e49dffa8
Uwe Dauernheim <uwe@klarna.com>
2011-08-02 12:48:35 +0200

    Cleanup

--- src/kanim_obfuscated.c
+++ src/kanim_obfuscated.c
@@ -3,23 +3,3 @@ s(x){x*x;}c(i){s(0.34*(x+l)+23-C(i))+s(2.0*y-7-C(i+1))<=s(C(i+2)-8*(1-s(1-f/
 5e1))+t);}main(){for(;f--;f&&puts("\x1b[19A")&usleep(20000))for(y=0;y<18;y++
 ,putchar(10))for(x=j=0;x<46*3;x+=3,putchar(" ..).\\/8"[j]))for(l=k=0;l<3;j^=
 k<<l++)for(i=0;i<42;i++)for(t=2;t--;)c(33)&c(36)&c(i)&c(i+3)?k=t:0;} // Djui
-
-/*
-f=50,x,y,i,j,k,l,t;C(i){" 5Pj+I *Sm7H+FBN$5 <Pa)<4#I~?Q-PFN&8K&8nE@"[i]-32;}
-s(x){x*x;}c(x,y,l,f,t,i){s(0.34*(x+l)+23-C(i))+s(2.0*y-7-C(++i))<s(C(++i)-8*(1-s(1-f/50.0))+t);}
-main(){for(;f--;f&&puts("\x1b[19A")&usleep(20000))
-for(y=0;y<18;y++,putchar(10))
-for(x=j=0;x<46*3;x+=3,putchar(" ..).\\/8"[j]))
-for(l=k=0;l<3;j^=k<<l++)
-for(i=0;i<42;i++)
-for(t=2;t--;)c(x,y,l,f,t,33)&&c(x,y,l,f,t,36)&&c(x,y,l,f,t,i)&&c(x,y,l,f,t,i+3)?k=t:0;}
-*/
-
-/*
-c,p,i,j,n,F=40,k,m;float a,x,y,S=0,V=0;main(){for(;F--;usleep(50000),F?puts(
-"\x1b[25A"):0)for(S+=V+=(1-S)/10-V/4,j=0;j<72;j+=3,putchar(10))for(i=0;x=S*(
-i-27),i++<73;putchar(c[" ''\".$u$"]))for(c=0,n=3;n--;)for(y=S*(j+n-36),k=0,c
-^=(136*x*x+84*y*y<92033)<<n,p=6,m=0;m<8;k++["<[\\]O=IKNAL;KNRbF8EbGEROQ@BSX"
-"XtG!#t3!^"]/1.16-68>x*cos(a)+y*sin(a)?k=p,p="<AFJPTX"[m++]-50:k==p?c^=1<<n,
-m=8:0)a=(k["O:85!fI,wfO8!yZfO8!f*hXK3&fO;:O;#hP;\"i[by asloane"]-79)/14.64;}
-*/
