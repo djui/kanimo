946f9d1847a8ec8c17b0e11b21ac3057fd391f89
Uwe Dauernheim <uwe@klarna.com>
2011-08-04 00:21:01 +0200

    Update pixel brightness pattern

--- src/kanimo.c
+++ src/kanimo.c
@@ -21,7 +21,7 @@ int X[]={
 f=50,x,y,i,j,k,l,t;C(i){return X[i]-32
 ;}s(float x){return x*x;}c(i){return s(0.34*(x+l)+23-C(i))+s(2*y-f/8-8-C(i+1))<=
 s(C(i+2)+t-0.16*f);}main(){for(;f--;f&&puts("\e[20A")&usleep(20000))for(y=0;y<19
-;y++,puts(""))for(x=0;x<138;x+=3,putchar(" ..).|(8"[j]))for(l=j=0;l<3;j^=k<<l++)
+;y++,puts(""))for(x=0;x<138;x+=3,putchar(" )v)(^(8"[j]))for(l=j=0;l<3;j^=k<<l++)
 for(i=0;i<42;i+=6)for(t=2;t--;)c(33)&c(36)&c(i)&c(i+3)?k=!t:0;}//by Djui&Jenster
 
 //              .                             
